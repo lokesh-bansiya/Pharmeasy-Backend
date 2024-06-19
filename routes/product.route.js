fb9c23018c3eab4441a315af806aed4a445e6d1f
Lokesh Patidar <lokeshbansiya29988@gmail.com>
2024-06-19 17:57:51 +0530

    Add Code

---
+++ routes/product.route.js
@@ -0,0 +1,68 @@
+const express = require("express");
+const jwt = require("jsonwebtoken");
+const ProductRoute = express.Router();
+const { ProductModel } = require("../models/product.model");
+
+ProductRoute.get("/", async (req, res) => {
+  const AllUsers = await ProductModel.find();
+  res.send(AllUsers);
+});
+
+ProductRoute.post("/", async (req, res) => {
+  const NewProduct = new ProductModel(req.body);
+  await NewProduct.save();
+  res.send({ msg: "Product added successfully" });
+});
+
+ProductRoute.patch("/:ID", async (req, res) => {
+  const { ID } = req.params;
+  y;
+
+  ProductModel.findByIdAndUpdate(ID, req.body, function (err, docs) {
+    if (err) {
+      res.send({ msg: "Error in Updating data" });
+    } else {
+      res.send({ msg: "Product Updated Successfully" });
+    }
+  });
+});
+
+ProductRoute.delete("/:ID", async (req, res) => {
+  const { ID } = req.params;
+
+  ProductModel.findByIdAndDelete(ID, function (err, docs) {
+    if (err) {
+      res.send({ msg: "Error in deleting data" });
+    } else {
+      res.send({ msg: "Product Deleted Successfully" });
+    }
+  });
+});
+
+// Function to add an array of products
+ProductRoute.post("/addMany", async (req, res) => {
+  try {
+    const products = req.body; // Assuming the products array is sent in the request body
+
+    // Validate the request body to ensure it's an array of objects
+    if (!Array.isArray(products)) {
+      return res.status(400).send({
+        message: "Invalid data format. Expected an array of products.",
+      });
+    }
+
+    // Insert the products into the database
+    const insertedProducts = await ProductModel.insertMany(products);
+    res.status(201).send({
+      message: "Products added successfully",
+      insertedProducts,
+    });
+  } catch (error) {
+    console.error("Error adding products:", error);
+    res
+      .status(500)
+      .send({ message: "An error occurred while adding products." });
+  }
+});
+
+module.exports = { ProductRoute };
---
+++ routes/singup.route.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const bcrypt = require("bcrypt");
+const SignupRoute = express.Router();
+const { UserModel } = require("../models/user.model");
+const { SignupMiddleware } = require("../middlewares/signupMiddleWare");
+
+SignupRoute.post("/", SignupMiddleware, async (req, res) => {
+  const { Email, Password, FullName } = req.body;
+  console.log("Check", req.body);
+  const hashedPass = await bcrypt.hash(Password, 6);
+  var Type;
+
+  if (Email.includes("@masaischool.com")) {
+    Type = "Admin";
+  } else {
+    Type = "User";
+  }
+
+  const newUser = new UserModel({
+    Email,
+    Password: hashedPass,
+    FullName,
+    Type,
+  });
+  await newUser.save();
+  res.send({ msg: "SignUp successful" });
+});
+
+module.exports = { SignupRoute };
